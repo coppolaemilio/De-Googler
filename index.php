affe53cc2e6d44bc569a678b03f4e88c60440cd7
FranciscoP <pakito15191@hotmail.com>
2013-03-01 18:53:22 +0100

    Commented
    Commented the code to make it more readable

--- index.php
+++ index.php
@@ -1,35 +1,47 @@
 <?php
 include dirname(__FILE__) . '/pfopen.php';
 
-if ($fp = fopen (dirname(__FILE__) . '/proxies', 'r'))
-  while ($line = fgets ($fp))
-    {
-    $p = explode(' ', $line);
-    $inter['url'] = $p[0];
-    if (!empty($p[1]))
-      $inter['port'] = $p[1];
-
-    $proxy[] = $inter;
-    }
-
+// When the user tries to access a page.
 if (!empty($_GET))
   {
+  // Retrieve all the proxies in the array $proxy
+  if ($fp = fopen (dirname(__FILE__) . '/proxies', 'r'))
+    // While there are proxies
+    while ($line = fgets ($fp))
+      {
+      // Separates each line in it's proxy and port for that proxy and saves it in an array
+      $p = explode(' ', $line);
+      
+      // We need this auxiliar array or $proxy won't be an array of arrays.
+      $aux['url'] = $p[0];
+      if (!empty($p[1]))
+        $aux['port'] = $p[1];
+
+      // Each element of proxy is an array, the first one being 'url' and the second one being 'port'
+      $proxy[] = $inter;
+      }
+    
   $i = 0;
+  // Attempt to open google as long as there are proxies available
   while(!empty($proxy[$i]['url']) &&
         !($fh = pfopen('http://google.com/search?q=' . $_POST['q'] . '&btnI',
                        $proxy[$i]['url'],
                        $proxy[$i]['port'])))
     {
     $details = stream_get_meta_data($fh);
-  
+    
     foreach ($details['wrapper_data'] as $line)
       if (is_string($line) && preg_match('/^Location: (.*?)$/i', $line, $m))
         $page =  $m[1];
     }
+  // Yay! It works! Go there then!
   if (!empty($page))
     header ('Location: ' . $page);
+  
+  // When there's no url after trying to get it 'so hard'
   else
     include dirname(__FILE__) . '/empty.php';
   }
+// The main page
 else
   include dirname(__FILE__) . '/main.php';
