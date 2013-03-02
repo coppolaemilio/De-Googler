78248868ffa3039307f9a25560b81b1be82654b3
FranciscoP <pakito15191@hotmail.com>
2013-03-02 19:09:45 +0100

    Retrieve the last url from the passed opened file

---
+++ retrieve.php
@@ -0,0 +1,12 @@
+<?php
+
+// Retrieve the last url from the meta data
+function retrieve_url ($file)
+  {
+  $details = stream_get_meta_data($file);
+      
+  foreach ($details['wrapper_data'] as $line)
+    if (is_string($line) && preg_match('/^Location: (.*?)$/i', $line, $m))
+      $page = $m[1];
+  return $page;
+  }
