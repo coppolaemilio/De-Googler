cb1420a8dfc545663503ae8c22726e8c872f8948
FranciscoP <pakito15191@hotmail.com>
2013-03-02 19:15:09 +0100

    Retrieve only the google-free url

--- retrieve.php
+++ retrieve.php
@@ -1,12 +1,14 @@
 <?php
 
 // Retrieve the last url from the meta data
-function retrieve_url ($file)
+function retrieve ($file)
   {
   $details = stream_get_meta_data($file);
       
   foreach ($details['wrapper_data'] as $line)
     if (is_string($line) && preg_match('/^Location: (.*?)$/i', $line, $m))
       $page = $m[1];
-  return $page;
+      
+  if (strpos($page, 'google') === FALSE)
+    return $page;
   }
