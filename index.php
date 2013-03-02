219832db50bafcc7baac7e393d3a008593197404
FranciscoP <pakito15191@hotmail.com>
2013-03-02 19:39:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+// Variables
 $proxiename = 'proxies';
 if (!empty($_GET))
   $googleurl = 'http://google.com/search?q=' . $_GET['q'] . '&btnI';
 
+// Require all the used files. TODO: __autoload()
 foreach (array('pfopen', 'retrieve') as $ToRequire)
   if (file_exists(dirname(__FILE__) . '/' . $ToRequire . '.php'))
     require dirname(__FILE__) . '/' . $ToRequire . '.php';
@@ -15,32 +17,28 @@ if (!empty($_GET))
     $page = retrieve ($fh);
   
   // You don't like it? Take the proxies then, google!
-  if (empty($page))
+  if (empty($page) && $fp = fopen (dirname(__FILE__) . '/' . $proxiename, 'r'))
     {
-    // Retrieve all the proxies in the array $proxy
-    if ($fp = fopen (dirname(__FILE__) . '/' . $proxiename, 'r'))
-      // While there are proxies
-      while ($line = fgets ($fp))
-        {
-        // Separates each line in it's proxy and port for that proxy and saves it in an arrayva
-       $p = explode(' ', $line);
-        
-        // We need this auxiliar array or $proxy won't be an array of arrays.
-        $aux['url'] = $p[0];
-        if (!empty($p[1]))
-          $aux['port'] = $p[1];
-        // Each element of proxy is an array, the first one being 'url' and the second one being 'port'
-        $proxy[] = $inter;
-        }
+    // Each proxy is stored in a line
+    while ($line = fgets ($fp))
+      {
+      // Separates each line in it's proxy and port for that proxy and saves it in an array
+      $p = explode(' ', $line);
+      
+      // We need this auxiliar array or $proxy won't be an array of arrays.
+      $aux['url'] = (!empty($p[0])) ? $p[0] : null;
+      $aux['port'] = (!empty($p[1])) ? $p[1] : null;
+      
+      // Each element of $proxy is an array, the first one being 'url' and the second one being 'port'
+      $proxy[] = $inter;
+      }
     
     // Attempt to open google as long as there are proxies available
     foreach ($proxy as $aproxy)
-      {
-      if ($fh = pfopen('http://google.com/search?q=' . $_GET['q'] . '&btnI',
-                         $proxy[$i]['url'],
-                         $proxy[$i]['port']))
-      
-      }
+      // If a valid url is retrieved in $page
+      if ($fh = pfopen($googleurl, $aproxy['url'], $aproxy['port']) && $page = retrieve ($fh))
+        // Get out of the foreach loop
+        break;
     
     while(!empty($proxy[$i]['url']) &&
           !())
